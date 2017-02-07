4f7c8ea551ae41a9919111a0287dd973b2e659ad
clairellesage <clairellesage@gmail.com>
2017-02-07 05:24:58 +0000

    looks like compass template -- added handler to convert className into class (ES6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
+  	console.log("Rendering <App/>")
     return (
-      <h1>Hello React :)</h1>
+      <div className="wrapper">
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <MessageList>
+          <Message></Message>
+        </MessageList>
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/Message.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+  	console.log("Rendering <Message/>")
+    return (
+     <div class="message">
+      <span class="username">Anonymous1</span>
+      <span class="content">I won't be impressed with technology until I can download food.</span>
+    </div>
+    );
+  }
+}
+export default Message;
